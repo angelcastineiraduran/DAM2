d0514249ad501bf554de89851996c2899069095f
acastineiraduran <acastineiraduran@gmail.com>
2023-10-04 22:11:38 +0200

    kotlin matrices bucles etc teoria

--- Apuntes/Android/Kotlin/app/src/main/java/com/example/introduccion/MainActivity.kt
+++ Apuntes/Android/Kotlin/app/src/main/java/com/example/introduccion/MainActivity.kt
@@ -44,6 +44,31 @@ class MainActivity : AppCompatActivity() {
         var saludo2_1 = "Hola" + saldo2.toString() // forma correcta
 
         /**
+         * arrays
+         */
+        var matriz = arrayOf (
+            intArrayOf(1, 2, 3), // (0,0) (0,1) (0,2)
+            intArrayOf(4, 5, 6), // (1,0) (1,1) (1,2)
+            intArrayOf(7, 8, 9) // (2,0) (2,1) (2,2)
+            //intArrayOf(7, 8, 9, 10, 11) // puedo poner esto
+        )
+
+        recorrerMatriz(matriz)
+
+        /**
+         * colecciones
+         */
+        // INMUTABLE
+        var clientesVIP : Set<Int> = setOf(123, 3223, 423)
+        var setMezclados = setOf(123, "antonio", 423)
+        if (clientesVIP.contains(123)) println("123 es vip") // solo recibe int
+
+        // MUTABLE
+        var clientes : MutableSet<Int> = mutableSetOf(12, 323, 23)
+        //clientesVIP.add(1234) // error -> no mutable
+        clientes.add(123)
+        clientes.clear() // limpia todas
+        /**
          * Condicionales
          */
         var fecha : String = "01/02/2023"
@@ -59,6 +84,7 @@ class MainActivity : AppCompatActivity() {
             10, 11, 12 -> println("Estamos en otornio")
             else -> print("mes incorrecto")
         }
+
 
         /**
          * Bucles
@@ -76,6 +102,10 @@ class MainActivity : AppCompatActivity() {
             println("Te quedan ${intentos++} intentos")  // 1-printea, 2-incrementa
         }while(intentos < 2 && PIN_correcto != PIN_ingresado)
 
+        var recibos = arrayOf("luz", "agua", "gas")
+        recibos.set(0, "electricidad")
+        recorrerArray(recibos)
+
         /**
          * funciones = metodos
          */
@@ -88,9 +118,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /**
-     * Funciones
-     */
     fun mostrarDinero(){
         println("tienes 50 eur")
     }
@@ -103,5 +130,42 @@ class MainActivity : AppCompatActivity() {
         if(numero == 1) return true
         else return false
     }
+
+    fun recorrerArray(arreglo : Array<String>){
+        for (i in arreglo) // i de iterador
+            println(i)
+
+        println("--con indices--")
+        for (i in arreglo.indices) {
+            println(arreglo.get(i))
+        }
+        println("--tamanio arrar--")
+        for (i in 0 .. arreglo.size -1) {
+            println("${i + 1}: ${arreglo.get(i)}")
+        }
+    }
+
+    // forma propia
+    fun recorrerMatriz(matriz: Array<IntArray>){
+        for (i in 0 .. matriz.size - 1) { // incluye ultimo elemento (si matriz.size=3 iria incluido)
+            for (j in 0 .. matriz.get(i).size - 1){
+                print(matriz.get(i).get(j))
+            }
+            println("")
+
+        }
+    }
+
+    fun recorrerMatriz_v2 (matriz: Array<IntArray>) {
+        //for (i in 0 .. 2){  // correcto creo tambien
+        for (i in (0 until matriz.size)) { // No hace falta -1 pq NO incluye el ultimo
+             println()
+            for (j in (0 until matriz[i].size)) {
+                println("Poscicion[$i][$j]: ${matriz[i][j]}")
+            }
+        }
+    }
+
+
 
 }
