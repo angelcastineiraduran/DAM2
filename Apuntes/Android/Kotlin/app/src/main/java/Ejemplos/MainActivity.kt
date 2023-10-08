39181a8d94032694514ad76e3c8766be98a7c2f1
acastineiraduran <acastineiraduran@gmail.com>
2023-10-08 03:42:55 +0200

    poo kotlin

--- Apuntes/Android/Kotlin/app/src/main/java/Ejemplos/MainActivity.kt
+++ Apuntes/Android/Kotlin/app/src/main/java/Ejemplos/MainActivity.kt
@@ -1,18 +1,21 @@
-package POO
+package Ejemplos
 
+import POO.Person
+import POO.futbolista
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.introduccion.R
 
-class MainActivity : AppCompatActivity(){
+class MainActivity {class MainActivity : AppCompatActivity(){
     override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var angel : Person = Person("Angel", "32234233")
-        angel.passport = "dwieh"
-
+        var fur : futbolista = futbolista("hdwie","jbiu",3)
 
     }
+
+}
 
 }
--- Apuntes/Android/Kotlin/app/src/main/java/POO/Person.kt
+++ Apuntes/Android/Kotlin/app/src/main/java/POO/Person.kt
@@ -1,18 +1,28 @@
 package POO
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.introduccion.R
+
 //class Person { } // clase1 - ctr x defecto
 //class Person() { } // clase2 - ctr x defect
 //class Person(var nombre : String, var edad : Int) { } // - sin ctr x defec --> ctr param
 // clase1 == clase2 != clase3
 
 
-class Person (var name : String = "Antonio", var passport : String? = null, var edad : Int){
-    var alive: Boolean = true
-    fun die() {
-        alive = false
+open class Person (var name : String = "Antonio", var passport : String? = null){
+    fun setName(name: String){this.name = name}
+    open fun andar (){ // open para que pueda sobreescribirla
+        println("esta andando")
     }
 }
-
-
+class futbolista(name: String, passport: String?, var dorsal : Int) : Person (name, passport) {
+    fun velocidad(name: String, dorsal: Int) {
+        this.name = name
+    }
+    override fun andar(){ //override para que me deje sobreescribir
+        println("esta corriendo")
+    }
+}
 
 
