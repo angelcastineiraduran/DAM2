10446349f8e275b8e08d8cc0a6bce4431e0dd653
acastineiraduran <acastineiraduran@danielcastelao.org>
2023-10-24 13:27:59 +0200

    base relacional a entera

--- AD/26_baserelacionalATarefa/src/pkg26_baserelacionalatarefa/Main.java
+++ AD/26_baserelacionalATarefa/src/pkg26_baserelacionalatarefa/Main.java
@@ -15,9 +15,29 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        Metodos metodos = new Metodos();
+        boolean exito = false;
         
+//        // INSERTAR
+//        exito = metodos.insireProduto("112", "carne", 10.0, "13/05/23");
+//        System.out.println("exito = " + exito);
+        
+        
+//        // LISTAR
+//        exito = metodos.listaProdutos();
+        
+        // LISTAR POR CODIGO
+        exito = metodos.listaPorCodigo("112");
+        
+        // ACTUALIZAR REGISTRO
+        exito = metodos.actualizaPre("112", 4.0);
+        System.out.println("exito = " + exito);
+        
+        // ELIMANA PRODUTO
+        exito = metodos.eliminaProduto("111");
+        System.out.println("exito = " + exito);
     }
     
     
--- AD/26_baserelacionalATarefa/src/pkg26_baserelacionalatarefa/Metodos.java
+++ AD/26_baserelacionalATarefa/src/pkg26_baserelacionalatarefa/Metodos.java
@@ -10,17 +10,12 @@ import java.sql.*;
  *
  * @author postgres
  */
-
 public class Metodos {
-    
-    
-    
-    String nombreTabla;
+
+    String nombreTabla = "produtos";
     String nombreColumnas; // si son varios separar por comas
-    String nombreValores; // ""
-    
-    private final String INSERTAR_FILA_TABLA = "INSERT INTO " + nombreTabla + " (" + nombreColumnas + ") " + " VALUES " + " (" + nombreValores + ");" ;
-        
+    String nombreValores; // "" y entrecomillados con la simple "\'"
+
     public Connection conexion() throws SQLException {
         String driver = "jdbc:postgresql:";
         String host = "//localhost:"; // tamen poderia ser una ip como "192.168.1.14"
@@ -32,11 +27,106 @@ public class Metodos {
         Connection conn = DriverManager.getConnection(url, usuario, password);
         return conn;
     }
-    
-    public boolean nsireProduto(String codigo, String nome, Double prezo, String dataCaducidade){
-        nombreTabla = "";
-        nombreColumnas = "codigo, nome, prezo, dataCaducidade";
-        nombreValores = codigo + ", " + nome + ", " +  String.valueOf(prezo) + ", " + dataCaducidade;
+
+    public boolean insireProduto(String codigo, String descricion, Double prezo, String datac) {
+        nombreColumnas = "codigo, descricion, prezo, datac";
+        nombreValores = "'" + codigo + "'" + ", " + "'" + descricion + "'" + ", " + prezo + ", " + "'" + datac + "'";
+        String query = "INSERT INTO " + nombreTabla + " (" + nombreColumnas + ") " + " VALUES " + " (" + nombreValores + ");";
+        System.out.println(query);
+        try (Connection conn = conexion()) {
+            Statement statement = conn.createStatement();
+            int filasAfectadas = statement.executeUpdate(query);
+            return filasAfectadas > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean listaProdutos() throws SQLException {
+        try (Connection conn = conexion()) {
+            Statement statement = conn.createStatement();
+
+            // Consulta SQL para seleccionar todos los registros de la tabla
+            String consultaSQL = "SELECT * FROM " + nombreTabla;
+
+            ResultSet resultSet = statement.executeQuery(consultaSQL);
+
+            while (resultSet.next()) {
+                // Obtener los valores de cada columna en el registro
+                String codigo = resultSet.getString("codigo");
+                String descricion = resultSet.getString("descricion");
+                Double prezo = resultSet.getDouble("prezo");
+                String datac = resultSet.getString("datac");
+
+                // Imprimir o procesar los valores como desees
+                System.out.println("Código: " + codigo);
+                System.out.println("Descripción: " + descricion);
+                System.out.println("Precio: " + prezo);
+                System.out.println("Fecha: " + datac);
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
         return true;
+    }
+
+    public boolean listaPorCodigo(String codigoBuscado) throws SQLException {
+        String query = "SELECT * FROM " + nombreTabla + " WHERE codigo = " + "'" + codigoBuscado + "'";
+        System.out.println(query);
+        try (Connection conn = conexion()) {
+            Statement statement = conn.createStatement();
+            String consultaSQL = "SELECT * FROM " + nombreTabla + " WHERE codigo = " + "'" + codigoBuscado + "'";
+            ResultSet resultSet = statement.executeQuery(consultaSQL);
+            while (resultSet.next()) {
+                // Obtener los valores de cada columna en el registro
+                String codigo = resultSet.getString("codigo");
+                String descricion = resultSet.getString("descricion");
+                Double prezo = resultSet.getDouble("prezo");
+                String datac = resultSet.getString("datac");
+
+                // Imprimir o procesar los valores como desees
+                System.out.println("Código: " + codigo);
+                System.out.println("Descripción: " + descricion);
+                System.out.println("Precio: " + prezo);
+                System.out.println("Fecha: " + datac);
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
+    }
+
+    public boolean actualizaPre(String codigoBuscado, Double prezoNovo) throws SQLException {
+        String query = "UPDATE " + nombreTabla + " SET prezo = " + prezoNovo + " WHERE codigo = " + "'" + codigoBuscado + "';" ;
+        System.out.println(query);
+        try (Connection conn = conexion()) {
+            Statement statement = conn.createStatement();
+            String consultaSQL = "UPDATE " + nombreTabla + " SET prezo = " + prezoNovo + " WHERE codigo = " + "'" + codigoBuscado + "';" ;
+            int filasAfectadas = statement.executeUpdate(consultaSQL);
+            return filasAfectadas > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean eliminaProduto(String codigoBuscado) throws SQLException {
+        String query = "DELETE FROM " + nombreTabla + " WHERE codigo = " + "'" + codigoBuscado + "'";
+        System.out.println(query);
+        try (Connection conn = conexion()) {
+            Statement statement = conn.createStatement();
+            String consultaSQL = "DELETE FROM " + nombreTabla + " WHERE codigo = " + "'" + codigoBuscado + "'";
+            int filasAfectadas = statement.executeUpdate(query);
+            return filasAfectadas > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
