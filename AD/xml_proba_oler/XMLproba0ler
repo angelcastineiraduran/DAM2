ae5227947d35aacf09c25621d06771afa67ad9e0
acastineiraduran <acastineiraduran@danielcastelao.org>
2023-11-08 14:31:00 +0100

    proba 0 leer

---
+++ AD/xml_proba_oler/XMLproba0ler
@@ -0,0 +1,30 @@
+/*
+XMLproba0ler
+
+Tratase de ler os contidos do documento XML que se xenerou no exercicio anterior usando os metodos apropiados.
+
+as clases a usar son moi similares:
+XMLInputFactory 
+XMLStreamReader
+
+metodos da clase XMLStreamreader:
+hasNext() : para preguntar se ainda quedan elementos a ler 
+next(): para ler o seguinte elemento
+getEventType() : retorna un enteiro que  indica o tipo de elemento leido. Este enteiro
+ correspondese con unha constante definida por a interface XMLStreamConstants e que pode ser una das seguintes:
+
+                   START_ELEMENT
+                   END_ELEMENT
+                   ATTRIBUTE
+                   CHARACTERS
+                   COMMENT
+                   SPACE
+                   DTD
+
+para preguntar por exemplo se o tipo de evento leido e un Start Element  preguntaremos se este e igual a  XMLStreamConstants.START_ELEMENT:
+getLocalName():devolta o nome do elemento actual
+getAttributeValue(posidion_do_atributo_dende_0):  devolta un string con o atributo almacenado no indice especificado
+getElementText() : devolta o valor de un elemento de texto
+
+*/
+
---
+++ AD/xml_proba_oler/src/xml_proba_oler/Main.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package xml_proba_oler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+/**
+ *
+ * @author dam2
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+        
+        String ruta = "/home/dam2/DAM2/AD/xml_proba_0/prueba.xml";
+        FileReader fr = new FileReader(new File(ruta));
+        
+        XMLInputFactory xmlif = XMLInputFactory.newInstance();
+        XMLStreamReader xmlsr = xmlif.createXMLStreamReader(fr);
+        
+        int tipoEvento;
+        
+        while(xmlsr.hasNext()){
+            
+            tipoEvento = xmlsr.next();
+            String nombreElemento;
+            String nombreAtributo;
+            String nombreContenido;
+            
+            if(tipoEvento == XMLStreamConstants.START_ELEMENT) {
+                nombreElemento = xmlsr.getLocalName();
+                
+                if(nombreElemento.equals("autor")) {
+                    System.out.println(nombreElemento + "\t");
+                    
+                    nombreAtributo = xmlsr.getAttributeValue(0);
+                    System.out.println("\t" + nombreAtributo);
+                    
+                } else if(nombreElemento.equals("nombre")) {
+                    System.out.println("\t" + nombreElemento);
+                    
+                    nombreContenido = xmlsr.getElementText();
+                    System.out.println("\t\t" + nombreContenido);
+                } else if(nombreElemento.equals("titulo")) {
+                    System.out.println("\t" +nombreElemento);
+                    
+                    nombreContenido = xmlsr.getElementText();
+                    System.out.println("\t\t" +nombreContenido);
+                    
+                }
+            }
+        }
+        
+        
+    }
+    
+}
